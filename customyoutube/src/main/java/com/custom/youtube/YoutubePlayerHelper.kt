98277fa69272500ecec808288f8fae76687db50f
NIRAJ0009 <niraj@appsquadz.co>
2026-02-13 12:42:49 +0530

    Release 1.0.6

--- customyoutube/src/main/java/com/custom/youtube/YoutubePlayerHelper.kt
+++ customyoutube/src/main/java/com/custom/youtube/YoutubePlayerHelper.kt
@@ -23,20 +23,44 @@ public class YoutubePlayerHelper(private val webView: WebView) {
             "https://admin.videocrypt.in/youtube/$videoId?autoplay=1&vq=hd1080&enablejsapi=1"
 
         val html = """
-            <!DOCTYPE html>
-            <html>
-            <body style="margin:0;padding:0;background:black;">
-                <iframe 
-                    width="100%" 
-                    height="100%" 
-                    src="$youtubeUrl"
-                    frameborder="0"
-                    allow="autoplay; encrypted-media"
-                    allowfullscreen>
-                </iframe>
-            </body>
-            </html>
-        """.trimIndent()
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style>
+            html, body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                height: 100%;
+                background: black;
+            }
+            .video-container {
+                position: relative;
+                width: 100%;
+                height: 100%;
+            }
+            .video-container iframe {
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                border: 0;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="video-container">
+            <iframe
+                src="$youtubeUrl"
+                allow="autoplay; encrypted-media"
+                allowfullscreen>
+            </iframe>
+        </div>
+    </body>
+    </html>
+""".trimIndent()
 
         webView.loadDataWithBaseURL(
             "https://www.youtube.com",
