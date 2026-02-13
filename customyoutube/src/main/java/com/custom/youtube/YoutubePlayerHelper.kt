57257376619cd03eb439f60145c2f3f3cf5d3027
NIRAJ0009 <niraj@appsquadz.co>
2026-02-13 11:29:23 +0530

    Release 1.0.4

---
+++ customyoutube/src/main/java/com/custom/youtube/YoutubePlayerHelper.kt
@@ -0,0 +1,49 @@
+package com.custom.youtube
+
+import android.webkit.WebSettings
+import android.webkit.WebView
+
+class YoutubePlayerHelper(private val webView: WebView) {
+
+    init {
+        setupWebView()
+    }
+
+    private fun setupWebView() {
+        val settings: WebSettings = webView.settings
+        settings.javaScriptEnabled = true
+        settings.domStorageEnabled = true
+        settings.loadWithOverviewMode = true
+        settings.useWideViewPort = true
+        settings.mediaPlaybackRequiresUserGesture = false
+    }
+
+    fun loadVideo(videoId: String) {
+        val youtubeUrl =
+            "https://admin.videocrypt.in/youtube/$videoId?autoplay=1&vq=hd1080&enablejsapi=1"
+
+        val html = """
+            <!DOCTYPE html>
+            <html>
+            <body style="margin:0;padding:0;background:black;">
+                <iframe 
+                    width="100%" 
+                    height="100%" 
+                    src="$youtubeUrl"
+                    frameborder="0"
+                    allow="autoplay; encrypted-media"
+                    allowfullscreen>
+                </iframe>
+            </body>
+            </html>
+        """.trimIndent()
+
+        webView.loadDataWithBaseURL(
+            "https://www.youtube.com",
+            html,
+            "text/html",
+            "UTF-8",
+            null
+        )
+    }
+}
